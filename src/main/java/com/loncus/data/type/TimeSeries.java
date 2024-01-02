1dd885e18bc2ab3b78dcccd4e9ff73e1144f0e28
霍寅松 <2507648942@qq.com>
2024-01-02 14:01:05 +0800

    chore: 添加注释

--- src/main/java/com/loncus/data/type/TimeSeries.java
+++ src/main/java/com/loncus/data/type/TimeSeries.java
@@ -4,14 +4,23 @@ import java.math.BigDecimal;
 import java.util.*;
 import lombok.NonNull;
 
-/** The type Time series. */
+/**
+ * This class represents a time series, a sequence of numerical data points in successive order.
+ */
 public class TimeSeries {
+  // A list of numerical data points
   private final List<BigDecimal> series;
 
   private final NavigableSet<Time> times = new TreeSet<>();
 
   private final Map<Time, Integer> timeToIndexMap;
 
+  /**
+   * Private constructor for creating a TimeSeries object.
+   *
+   * @param timeList a list of time points
+   * @param values a list of numerical data points
+   */
   private TimeSeries(final List<Time> timeList, final List<BigDecimal> values) {
     this.series = values;
 
@@ -28,6 +37,13 @@ public class TimeSeries {
     this.timeToIndexMap = Collections.unmodifiableMap(timeToIndexMap);
   }
 
+  /**
+   * Factory method for creating a TimeSeries object from a list of dates and values.
+   *
+   * @param dates a list of dates
+   * @param values a list of numerical data points
+   * @return a new TimeSeries object
+   */
   public static TimeSeries from(
       @NonNull final List<Date> dates, @NonNull final List<BigDecimal> values) {
     List<Time> times = new ArrayList<>(dates.size());
@@ -38,25 +54,43 @@ public class TimeSeries {
     return new TimeSeries(times, values);
   }
 
+  /**
+   * Factory method for creating a TimeSeries object from a list of Time objects and values.
+   *
+   * @param times a list of Time objects
+   * @param values a list of numerical data points
+   * @return a new TimeSeries object
+   */
   public static TimeSeries fromTimes(
       @NonNull final List<Time> times, @NonNull final List<BigDecimal> values) {
 
     return new TimeSeries(times, values);
   }
 
+  /**
+   * Returns a copy of the data points in the time series.
+   *
+   * @return a list of numerical data points
+   */
   public List<BigDecimal> getValues() {
     return new ArrayList<>(this.series);
   }
 
+  /**
+   * Returns the time points in the time series.
+   *
+   * @return a set of time points
+   */
   public final NavigableSet<Time> getTimes() {
     return this.times;
   }
 
   /**
-   * get time series values by index
+   * Returns the data point at the specified index.
    *
-   * @param index the index
-   * @return the BigDecimal
+   * @param index the index of the data point
+   * @return the data point at the specified index
+   * @throws IndexOutOfBoundsException if the index is out of range
    */
   public final BigDecimal at(final int index) {
     if (index < 0 || index >= this.series.size()) {
@@ -66,11 +100,12 @@ public class TimeSeries {
   }
 
   /**
-   * get time series values by time
+   * Returns the data point at the specified time.
    *
-   * @param time the time
-   * @param autoComplete the auto complete
-   * @return the BigDecimal
+   * @param time the time of the data point
+   * @param autoComplete if true, returns the data point at the previous time if the specified time does not exist
+   * @return the data point at the specified time
+   * @throws IllegalArgumentException if the time does not exist in the time series
    */
   public final BigDecimal at(@NonNull final Time time, Boolean autoComplete) {
     if (!timeToIndexMap.containsKey(time)) {
@@ -89,20 +124,22 @@ public class TimeSeries {
   }
 
   /**
-   * get time series values by time without auto complete
+   * Returns the data point at the specified time.
    *
-   * @param time the time
-   * @return the BigDecimal
+   * @param time the time of the data point
+   * @return the data point at the specified time
+   * @throws IllegalArgumentException if the time does not exist in the time series
    */
   public final BigDecimal at(@NonNull final Time time) {
     return at(time, false);
   }
 
   /**
-   * This method is used to add a new data point to the TimeSeries object.
+   * Adds a new data point to the time series.
    *
-   * @param time The time of the new data point.
-   * @param value The value of the new data point.
+   * @param time the time of the new data point
+   * @param value the new data point
+   * @throws IllegalArgumentException if the time already exists in the time series
    */
   public void add(Time time, BigDecimal value) {
     if (timeToIndexMap.containsKey(time)) {
@@ -114,13 +151,11 @@ public class TimeSeries {
   }
 
   /**
-   * This method is used to slice a TimeSeries object. It creates a new TimeSeries object that
-   * includes all data points from the start time to the end time.
+   * Returns a new TimeSeries object that is a slice of this time series between the specified start and end times.
    *
-   * @param start The start time of the slice. This is inclusive.
-   * @param end The end time of the slice. This is inclusive.
-   * @return A new TimeSeries object that includes all data points from the start time to the end
-   *     time.
+   * @param start the start time of the slice
+   * @param end the end time of the slice
+   * @return a new TimeSeries object that is a slice of this time series
    */
   public TimeSeries slice(Time start, Time end) {
     NavigableSet<Time> newTimes = times.subSet(start, true, end, true);
--- src/main/java/com/loncus/data/type/TimeSeriesPoint.java
+++ src/main/java/com/loncus/data/type/TimeSeriesPoint.java
@@ -2,27 +2,59 @@ package com.loncus.data.type;
 
 import java.math.BigDecimal;
 
+/**
+ * This class represents a point in a time series.
+ * A time series point consists of a time and a value from a time series at that time.
+ */
 public class TimeSeriesPoint {
   private TimeSeries timeSeries;
+
   private Time time;
 
+  /**
+   * Constructs a new TimeSeriesPoint with the specified time series and time.
+   *
+   * @param timeSeries the time series to which this point belongs
+   * @param time the time at which this point exists
+   */
   public TimeSeriesPoint(TimeSeries timeSeries, Time time) {
     this.timeSeries = timeSeries;
     this.time = time;
   }
 
+  /**
+   * Returns the time series to which this point belongs.
+   *
+   * @return the time series to which this point belongs
+   */
   public TimeSeries getTimeSeries() {
     return timeSeries;
   }
 
+  /**
+   * Returns the time at which this point exists.
+   *
+   * @return the time at which this point exists
+   */
   public Time getTime() {
     return time;
   }
 
+  /**
+   * Returns the value of this point from the time series at its time.
+   *
+   * @return the value of this point from the time series at its time
+   */
   public BigDecimal getValue() {
     return timeSeries.at(time, true);
   }
 
+  /**
+   * Sets the time at which this point exists and returns this point.
+   *
+   * @param time the new time at which this point should exist
+   * @return this point
+   */
   public TimeSeriesPoint setTime(Time time) {
     this.time = time;
     return this;
